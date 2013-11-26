c1bea5560b382cfe8dda667022cda6ab08df339e
Januar <januarfonti01@gmail.com>
2013-11-26 11:09:22 +0700

    tambah menu manage user

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -282,15 +282,124 @@ class Admin extends CI_Controller {
   
 	function manage_user()
 	{
-
-			$data['data_profile'] = $this->model_native->get_profile();
-			$data['judul']	    ='Ubah Profil';
-			$html_title ="Manage User";
-			$this->load->vars( array( 'html_title' => $html_title) );
-			$data['output']	=$this->load->view('admin/manage_user',$data,TRUE);
-			$this->load->view('admin/content_wrapper_home',$data);
+		if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+			{
+				$data['data_profile'] = $this->model_native->get_profile();
+				$data['judul']	    ='Ubah Profil';
+				$html_title ="Manage User";
+				$this->load->vars( array( 'html_title' => $html_title) );
+				$data['output']	=$this->load->view('admin/manage_user',$data,TRUE);
+				$this->load->view('admin/content_wrapper_home',$data);
+			}
+			else {
+				redirect('auth');		
+			}
 			
 	}
+
+	function add_user()
+	{
+
+			if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+			{
+				$d['id_param'] = "";
+				$d['username'] = ""; 
+				$d['password'] = ""; 
+				$d['nama_lengkap'] = ""; 
+				$d['st'] = "tambah";
+				//$this->load->view('dashboard_admin/user/input',$d);
+				$d['output']	=$this->load->view('admin/add_user',$d,TRUE);
+				$html_title ="Add User";
+				$this->load->vars( array( 'html_title' => $html_title) );
+				$this->load->view('admin/content_wrapper_home',$d);
+			}
+			else
+			{
+				redirect('auth');
+			}
+	}
+
+	public function simpan()
+	{
+		if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+		{
+
+
+			$in['username'] = $this->input->post("username");
+						$in['nama_lengkap'] = $this->input->post("nama_lengkap");
+						$in['stts'] = "administrator";
+						$in['password'] = md5($this->input->post("password").'AppSimpeg32');
+						$this->db->insert("tbl_user_login",$in);
+						redirect('admin/manage_user');
+
+		}
+		else {
+			redirect('auth');
+		}
+			
+	}
+
+	public function hapus()
+	{
+		if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+		{
+			$id['id_user_login'] = $this->uri->segment(3);
+			$this->db->delete("tbl_user_login",$id);
+			redirect('admin/manage_user');
+		}
+		else
+		{
+			redirect('auth');
+		}
+	} 
+
+	public function edit()
+	{
+		if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+		{
+			$id['id_user_login'] = $this->uri->segment(3);
+			$q = $this->db->get_where("tbl_user_login",$id);
+			$d = array();
+			foreach($q->result() as $dt)
+			{
+				$d['id_user_login'] = $dt->id_user_login;
+				$d['username'] = $dt->username; 
+				$d['password'] = $dt->password; 
+				$d['nama_lengkap'] = $dt->nama_lengkap; 
+			}
+			$d['st'] = "edit";
+			$d['output']	=$this->load->view('admin/edit_user',$d,TRUE);
+			$html_title ="NATIVE SPEAKER Language School - Edit Profile";
+			$this->load->vars( array( 'html_title' => $html_title) );
+			$this->load->view('admin/content_wrapper_home',$d);
+		}
+		else
+		{
+			redirect('auth');
+		}
+	}
+
+	public function simpan_edit()
+	{
+		if($this->session->userdata('logged_in')!="" && $this->session->userdata('stts')=="administrator")
+		{
+			//$id['id_user_login'] = $this->uri->segment(3);
+			$id['id_user_login'] = $this->input->post("id_user_login");
+			$upd['username'] = $this->input->post("username");
+			$upd['nama_lengkap'] = $this->input->post("nama_lengkap");
+			if($this->input->post("password")!="")
+				{
+					$upd['password'] = md5($this->input->post("password").'AppSimpeg32');
+				}
+				$this->db->update("tbl_user_login",$upd,$id);
+				redirect('admin/manage_user');
+		}
+		else {
+			redirect('auth');
+		}
+	}
+
+	
 
 	
 
